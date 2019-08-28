84a779c96bac3127794728647cac413fe0796edc
Andrzej Witas <andrzej.witas@capgemini.com>
2019-08-28 14:36:28 +0200

    Add Views

---
+++ XamarinProj/XamarinProj/ContactView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinProj
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ContactView : ContentPage
+	{
+		public ContactView ()
+		{
+			InitializeComponent ();
+		}
+
+        private void SendMessageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ContactMeSwitch_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ XamarinProj/XamarinProj/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinProj.Models
+{
+    public class Item
+    {
+        public Item(string itemName, string description, double price, string imageUrl, bool inStock)
+        {
+            Id = Guid.NewGuid();
+            ItemName = itemName ?? throw new ArgumentNullException(nameof(itemName));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            Price = price;
+            ImageUrl = imageUrl ?? throw new ArgumentNullException(nameof(imageUrl));
+            InStock = inStock;
+        }
+
+        public Guid Id { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool InStock { get; set; }
+    }
+
+}
+}
